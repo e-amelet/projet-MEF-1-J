37c68c8761fe7b7f5d71c80468402751e6dacb98
Gorren0 <jeremie.rolland@etu.cyu.fr>
2026-05-07 11:23:30 +0200

    ez

--- game.h
+++ game.h
@@ -1,1 +1,45 @@
-sqdfsqdfsdfsqdfqsdf
+#ifndef GAME_H
+#define GAME_H
+#include <time.h>
+#define TAILLE_PLATEAU 5
+#define JOUEUR_MIN 2
+#define JOUEUR_MAX 4
+#define TAILLE_NOM_MAX 32
+#define STATS_MAX 200
+#define STATS_FILE "Stats_joueurs.txt"
+
+typedef enum{
+    Guerrier;
+    Ranger;
+    Mage;
+    Voleur;
+}ClassJoueur;
+
+typedef enum{
+    Bouclier;
+    Torche;
+    Hache;
+    Arc;
+}Arme;
+
+typedef enum{
+    Basilic;
+    Zombie;
+    Troll;
+    Harpie;
+    Trésor;
+    Portail ;
+    Totem;
+    Antique_Guerrier;
+    Antique_Ranger;
+    Antique_Mage;
+    Antique_Voleur;
+} TypeCase;
+typedef struct{
+    int ligne
+    int colonne
+}
+
+typedef struct {
+    
+}
